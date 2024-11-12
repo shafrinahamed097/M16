3ce52867c3f0b4ebe77df330ffd70f19f7bb4366
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-11-12 23:31:39 +0600

    Blade - switch case

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,4 +1,4 @@
-@if($result === 100)
+<!-- @if($result === 100)
   <h1>Result is hundred</h1>
 @elseif($result === 1000)
    <h1>Result Thousand</h1>
@@ -6,4 +6,23 @@
    <h1>Result Ten Thousand</h1>
    @else
      <h1>Result is not in our range</h1>
-     @endif
+     @endif -->
+
+
+     @switch($result)
+
+       @case(100)
+        <h1>Result is hundred</h1>
+        @break
+
+        @case(1000)
+        <h1>Result Thousand</h1>
+        @break
+
+        @case(10000)
+        <h1>Result Ten Thousand</h1>
+        @default
+        <h1>Result is not in our range</h1>
+
+
+     @endswitch
