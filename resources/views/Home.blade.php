5e04c4e4930ddec5fc89e671b1db7c4f674797c4
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-11-13 11:40:48 +0600

    Blade - Including Subview

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,30 +1,4 @@
-
-
-
-     <!DOCTYPE html>
-     <html lang="en">
-     <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Laravel App</title>
-      <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}"  rel="stylesheet" type="text/css">
-     </head>
-     <body>
-
-     <div class="container">
-      <div class="row d-flex justify-content-center">
-        <div class="col-6">
-          <button class="btn btn-success">Hello</button>
-          <img src="{{asset('images/laravel.jpeg')}}" alt="Logo">
-
-        </div>
-      </div>
-
-     </div>
-      
-
-
-     <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
-     </body>
-     </html>
-
+@include('components.Header')
+@include('components.Hero')
+@include('components.Team')
+@include('components.Footer')
---
+++ resources/views/components/Team.blade.php
@@ -0,0 +1,1 @@
+<h1>This is Team Section</h1>
